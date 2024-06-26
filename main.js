7539670cbee35170930abb4ba14fe8fb0030e56c
Aditya-he <adityasharmamtj55@gmail.com>
2024-06-26 18:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Ranbir";
+
+var last_name = "Kapoor";
+console.log(last_name);
+
+first_name.concat(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var a = 10;
+var b = 5;
